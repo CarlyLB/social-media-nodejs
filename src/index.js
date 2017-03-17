02fdd00f8d6d4f18aaf3b8f718387a11fbe57fc3
Nikolay Grozev <nikolayg@au1.ibm.com>
2017-03-17 15:01:52 +1100

    Changed default port

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import bodyParser from 'body-parser';
 import ItemController from './ItemController';
 
 const app = express();
-const port: * = process.env.PORT || 3000;
+const port: * = process.env.PORT || 3001;
 const itemController = new ItemController();
 
 // Some middleware
